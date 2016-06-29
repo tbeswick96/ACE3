9ae01f554ee10508de7046099bc9a254a5269d67
Tim Beswick <contact.tim.here@gmail.com>
2016-06-30 00:25:13 +0100

    Customised Russian RHS compat

--- tools/make.py
+++ tools/make.py
@@ -1435,6 +1435,7 @@ See the make.cfg file for additional build options.
 if __name__ == "__main__":
     start_time = timeit.default_timer()
     main(sys.argv)
+    #main([sys.argv[0], 'compat_rhs_afrf3'])
     d,h,m,s = Fract_Sec(timeit.default_timer() - start_time)
     print("\nTotal Program time elapsed: {0:2}h {1:2}m {2:4.5f}s".format(h,m,s))
 
