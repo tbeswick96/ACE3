3e18f1d64663d01a0672c7462367329830238e8a
Tim Beswick <contact.tim.here@gmail.com>
2023-07-07 23:19:52 +0100

    Empty patches

--- optionals/compat_ws/config.cpp
+++ optionals/compat_ws/config.cpp
@@ -9,21 +9,21 @@
 // #ifdef PATCH_SKIP
 // ACE_PATCH_NOT_LOADED(ADDON,PATCH_SKIP)
 // #else
-// class CfgPatches {
-//     class ADDON {
-//         name = COMPONENT_NAME;
-//         units[] = {};
-//         weapons[] = {};
-//         requiredVersion = REQUIRED_VERSION;
-//         requiredAddons[] = {
-//             // "data_f_lxWS_Loadorder"
-//         };
-//         author = ECSTRING(common,ACETeam);
-//         authors[] = {"Mike"};
-//         url = ECSTRING(main,URL);
-//         VERSION_CONFIG;
-//     };
-// };
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            // "data_f_lxWS_Loadorder"
+        };
+        author = ECSTRING(common,ACETeam);
+        authors[] = {"Mike"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
 
 // #include "CfgWeapons.hpp"
 
