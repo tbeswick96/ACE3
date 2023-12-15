10056968de290a9a825780ce0e34d3795beee8a6
Tim Beswick <contact.tim.here@gmail.com>
2023-12-15 23:55:58 +0000

    Fix NV define

--- addons/nightvision/defines.hpp
+++ addons/nightvision/defines.hpp
@@ -1,4 +1,4 @@
 #define NVG_GREEN_PRESET GVAR(colorPreset)[] = {0, {0.0, 0.0, 0.0, 0.0}, {1.3, 1.2, 0.0, 0.9}, {6, 1, 1, 0.0}}
 #define NVG_WHITE_PRESET GVAR(colorPreset)[] = {0.0, {0.0, 0.0, 0.0, 0.0}, {1.1, 0.8, 1.9, 0.9}, {1, 1, 6, 0.0}}
 
-#define NVG_WHITE_PRESET_ARRAY = [0.0, [0.0, 0.0, 0.0, 0.0], [1.1, 0.8, 1.9, 0.9], [1, 1, 6, 0.0]];
+#define NVG_WHITE_PRESET_ARRAY [0.0, [0.0, 0.0, 0.0, 0.0], [1.1, 0.8, 1.9, 0.9], [1, 1, 6, 0.0]]
